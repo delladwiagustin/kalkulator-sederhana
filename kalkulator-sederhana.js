dde19d3ee1b30681c72aa5c3bd12b2b6d1b7b107
delladwiagustin <160448812+delladwiagustin@users.noreply.github.com>
2024-03-18 10:38:54 +0700

    Update kalkulator-sederhana.js

--- kalkulator-sederhana.js
+++ kalkulator-sederhana.js
@@ -3,3 +3,9 @@ function tambah(angka1, angka2) {
 }
 var hasilTambah = tambah(10, 20);
 console.log("Hasil penambahan: " + hasilTambah);
+
+function kurang(angka1, angka2) {
+    return angka1 - angka2;
+}
+var hasilKurang = kurang(5, 3);
+console.log("Hasil pengurangan: " + hasilKurang);
