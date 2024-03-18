f5a6abae1894241cee40d47971f3ea4f2d0cad89
delladwiagustin <160448812+delladwiagustin@users.noreply.github.com>
2024-03-18 11:26:36 +0700

    v1.2

--- kalkulator-sederhana.js
+++ kalkulator-sederhana.js
@@ -9,3 +9,9 @@ function kurang(angka1, angka2) {
 }
 var hasilKurang = kurang(10, 3);
 console.log("Hasil pengurangan: " + hasilKurang);
+
+function kali(angka1, angka2) {
+    return angka1 * angka2;
+}
+var hasilKali = kali(10, 3);
+console.log("Hasil perkalian: " + hasilKali);
